40434c5b196b1468bba0833e6024d05620ac8801
mycanhnguyen <canhmy04041998@gmail.com>
2022-11-09 00:05:06 +0700

    add build

---
+++ build/precache-manifest.4be904c7e26e02d3ff7b9e0fafc0d036.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "83b884d98c10abca51e5",
+    "url": "/static/css/main.ece66bf7.chunk.css"
+  },
+  {
+    "revision": "83b884d98c10abca51e5",
+    "url": "/static/js/main.83b884d9.chunk.js"
+  },
+  {
+    "revision": "c67734a803237e7b5671",
+    "url": "/static/js/1.c67734a8.chunk.js"
+  },
+  {
+    "revision": "ce976c00b62b49c9f9d9",
+    "url": "/static/css/2.11a4b137.chunk.css"
+  },
+  {
+    "revision": "ce976c00b62b49c9f9d9",
+    "url": "/static/js/2.ce976c00.chunk.js"
+  },
+  {
+    "revision": "da77f95c79fd008c81cf",
+    "url": "/static/js/runtime~main.da77f95c.js"
+  },
+  {
+    "revision": "c2b4885dbb41687a28ba2ebb9c813e02",
+    "url": "/static/media/my-cv.c2b4885d.pdf"
+  },
+  {
+    "revision": "3362d07dc28b93ec9d9d8c3e6d6a56d2",
+    "url": "/static/media/logo-name.3362d07d.png"
+  },
+  {
+    "revision": "1aed08b3e1b66991d921d25dbdcf28ed",
+    "url": "/static/media/home-main.1aed08b3.svg"
+  },
+  {
+    "revision": "3c9241c2d284b85346b0c8e719568989",
+    "url": "/static/media/avatar.3c9241c2.svg"
+  },
+  {
+    "revision": "8fb0e5fd721a02afda190255b5ee649d",
+    "url": "/static/media/about.8fb0e5fd.png"
+  },
+  {
+    "revision": "b588a538cb3ae6b60e91f5232436e1a6",
+    "url": "/static/media/lux-stay.b588a538.jpg"
+  },
+  {
+    "revision": "fc35551fdfae48b9c495b4b472beb29f",
+    "url": "/static/media/forkify-app.fc35551f.jpg"
+  },
+  {
+    "revision": "d097a13606c080eae9607a3b71c40d34",
+    "url": "/static/media/nhaccuatui.d097a136.jpg"
+  },
+  {
+    "revision": "776c2fcddc8d1336b51af49b8e7826a8",
+    "url": "/static/media/comfy-sloth.776c2fcd.jpg"
+  },
+  {
+    "revision": "44174c5d17174cbaba6c235c90bede73",
+    "url": "/static/media/portfolio.44174c5d.jpg"
+  },
+  {
+    "revision": "bccdfb7331cc9905c185c803946130da",
+    "url": "/static/media/shopee.bccdfb73.jpg"
+  },
+  {
+    "revision": "01180f2e7e6e149dc1efce97db7318f1",
+    "url": "/index.html"
+  }
+];
